060a79330d13041a56d9e3e0710c330a2f9b78cb
carloDM <carlo.de.mauro.work@gmail.com>
2023-06-01 16:43:57 +0200

    index message

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>laravel primi passi</title>
 </head>
 <body>
-    <p>index</p>
+    <p>{{$message}}</p>
     <a href="/home">home</a>
 </body>
 </html>
